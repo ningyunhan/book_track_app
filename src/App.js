75b34e51fe9456850a531217ce66ceb138523644
ningyunhan <ningyunhan19940829@gmail.com>
2017-12-19 00:10:14 -0500

    use state to load books

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import {Route} from 'react-router-dom'
 import AddBook from './AddBook'
@@ -7,13 +7,13 @@ import BookList from './BookList'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    //showSearchPage: false
+    books:[]
+  }
+
+  componentDidMount(){
+    BooksAPI.getAll().then((books) => {
+      this.setState({books})
+    });
   }
 
   render() {
@@ -24,7 +24,7 @@ class BooksApp extends React.Component {
         }>
         </Route>
         <Route exact path='/' render={() => 
-          <BookList/>
+          <BookList books={this.state.books}/>
         }>
         </Route>
       </div>
